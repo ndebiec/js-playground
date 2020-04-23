61933c935d8546aa5282dbb4ce40c5902fe8ee9f
ndebiec <nadia.debiec@netguru.pl>
2020-04-23 20:57:21 +0200

    Make countdown stop at 0

--- forest_countdown/index.js
+++ forest_countdown/index.js
@@ -2,6 +2,6 @@ var countdownElement = document.getElementById('countdown');
 var initialCountdownVal = countdownElement.innerHTML;
 
 setInterval(function() {
-  initialCountdownVal -= 1;
+  initialCountdownVal > 0 ? initialCountdownVal -= 1 : 0;
   countdownElement.innerHTML = initialCountdownVal
 }, 1000);
