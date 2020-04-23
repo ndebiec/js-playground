63ad613fff4cda5eb891dbf9c54d2f2eb4260213
ndebiec <nadia.debiec@netguru.pl>
2020-04-23 21:12:11 +0200

    Make background image change every sec

--- forest_countdown/index.js
+++ forest_countdown/index.js
@@ -1,7 +1,10 @@
 var countdownElement = document.getElementById('countdown');
 var initialCountdownVal = countdownElement.innerHTML;
+var bgImgPath = document.getElementById('bg-img');
 
 setInterval(function() {
-  initialCountdownVal > 0 ? initialCountdownVal -= 1 : 0;
+  initialCountdownVal > 0 ? initialCountdownVal -= 1 : 0; //should break/return when 0
   countdownElement.innerHTML = initialCountdownVal
+  var isEven = initialCountdownVal % 2 === 0
+  bgImgPath.src = isEven ? 'background-1.jpg' : 'background-2.jpg'
 }, 1000);
