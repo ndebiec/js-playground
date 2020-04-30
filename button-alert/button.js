d8d238682fc58f17bd24a0eb8730e5594afe8e3c
ndebiec <nadia.debiec@netguru.pl>
2020-04-30 09:22:05 +0200

    Add reColor function with random background triggerred on click

--- button-alert/button.js
+++ button-alert/button.js
@@ -1,8 +1,16 @@
 var btn = document.getElementById("thebutton");
+var mainDiv = document.querySelector("header div");
 
 function alertSomething() {
   alert("Something important");
 }
 
+function reColor() {
+  aCol = Math.round(Math.random()*255);
+  bCol = Math.round(Math.random()*255);
+  cCol = Math.round(Math.random()*255);
+  mainDiv.style.backgroundColor = "rgb(" + aCol + " , " + bCol + ", " + cCol + ")";
+}
+
 // btn.onclick = alertSomething;
-btn.addEventListener("click", alertSomething);
+btn.addEventListener("click", reColor);
