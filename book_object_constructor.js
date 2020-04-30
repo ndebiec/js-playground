4ae0e3fd04a65aa7f3b3cab07f578138b281cfd1
ndebiec <nadia.debiec@netguru.pl>
2020-04-30 23:29:25 +0200

    Extract function definition to prototype to avoid duplication

--- book_object_constructor.js
+++ book_object_constructor.js
@@ -3,11 +3,12 @@ function Book(title, pages, author, read) {
   this.pages = pages
   this.author = author
   this.read = read
-  this.info = function() {
-  let isRead = read ? ', already read.' : ', not read yet.'
-    console.log(title + " by " + author + ", " + pages + " pages" + isRead)
-  }
 }
+
+Book.prototype.info = function() {
+  let isRead = this.read ? ', already read.' : ', not read yet.'
+    console.log(this.title + " by " + this.author + ", " + this.pages + " pages" + isRead)
+  }
 
 const ann = new Book('Ann from Green Gables', '274', 'Lucy Maud Montgomery', true);
 
